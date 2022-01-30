6bb8799efc948b96a09a83365656ec1630f98ca7
mbobryshev-mac <mbob72@gmail.com>
2022-01-30 20:18:53 +0300

    HMR next step

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
 
 module.exports = {
-    mode: 'development',
     entry: {
-        index: './src/index.js'
+        app: './src/index.js',
+        // Runtime code for hot module replacement
+        hot: 'webpack/hot/dev-server.js',
+        // Dev server client for web socket transport, hot and live reload logic
+        client: 'webpack-dev-server/client/index.js?hot=true&live-reload=true',
     },
     devtool: 'inline-source-map',
     devServer: {
         static: './dist',
-        hot: true,
+        // Dev server client for web socket transport, hot and live reload logic
+        hot: false,
+        client: false,
     },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Hot Module Replacement',
         }),
+        // Plugin for hot module replacement
+        new webpack.HotModuleReplacementPlugin(),
     ],
     output: {
         filename: '[name].bundle.js',
